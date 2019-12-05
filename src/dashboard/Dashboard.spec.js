f244884cffd05f460d924bb49236ce530db82afa
Jake <panamajake86@gmail.com>
2019-12-04 20:39:49 -0700

    got the first test to pass

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
 // Test away
+import React from "react";
+// no default export, so we're importing everyting from this library
+import * as rtl from "@testing-library/react";
+// not importing to a variable, since this just overrides jest global variables
+import "@testing-library/jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+import { italic } from "ansi-colors";
+import expectExport from "expect";
+
+test('defaults to unlocked and open', () => {
+    const wrapper = rtl.render(<Dashboard />)
+    const locked = wrapper.getByTestId(/locked/i);
+    const closed = wrapper.getByTestId(/closed/i);
+    expect(locked).toBeEnabled();
+    expect(closed).toBeEnabled();
+});
